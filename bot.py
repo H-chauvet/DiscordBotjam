37d32529779802321fda5a99eaec25e17cf60a2c
Henri Chauvet <henri.chauvet@epitech.eu>
2022-12-19 15:35:35 +0100

    [ADD] Working bot with Christmas and Santa reply with ping and echo command

--- bot.py
+++ bot.py
@@ -1,14 +1,33 @@
-##
-## EPITECH PROJECT, 2022
-## DiscordBotjam
-## File description:
-## Create a BOT on the theme of ???
-##
+from discord.ext import commands
+import discord
 
-from discord import *
+intents = discord.Intents.all()
+prefix = "!"
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
-@client.event
 
+@bot.event
+async def on_ready():
+    print("Le bot est prêt !")
+
+@bot.event
 async def on_message(message):
-    if (message.content == "Christmas"):
-        print("Tree !")
+    if message.content == "Christmas":
+        await message.channel.send("Tree :D")
+    if message.content == "Santa":
+        await message.channel.send("Claus :D")
+    await bot.process_commands(message)
+
+
+@bot.command()
+async def ping(ctx):
+
+    latency = bot.latency
+    await ctx.send(latency)
+
+@bot.command()
+async def echo(ctx, *, content:str):
+    await ctx.send(content)
+
+
+bot.run('OTg1NjgwOTI5ODI3NzI5NDM5.Gm5cHb.n7Tt3mDOXdUHKc8UYPaDaHxKUc--MuUaSFXAWs')
